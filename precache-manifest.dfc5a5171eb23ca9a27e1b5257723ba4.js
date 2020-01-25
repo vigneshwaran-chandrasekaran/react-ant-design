6351114d0e289327a5d0c6eeb5599087cd75f033
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-01-25 19:14:15 +0530

    Updates

---
+++ precache-manifest.dfc5a5171eb23ca9a27e1b5257723ba4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c4297cf317fa1fef528fe1deef41e9",
+    "url": "/react-ant-design/index.html"
+  },
+  {
+    "revision": "9476110afc98d61b7bba",
+    "url": "/react-ant-design/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "1b7599356b2ef6cac405",
+    "url": "/react-ant-design/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9476110afc98d61b7bba",
+    "url": "/react-ant-design/static/js/2.a39fd500.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-ant-design/static/js/2.a39fd500.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b7599356b2ef6cac405",
+    "url": "/react-ant-design/static/js/main.820bbfda.chunk.js"
+  },
+  {
+    "revision": "97ca161f44c944d1bbec",
+    "url": "/react-ant-design/static/js/runtime-main.abf58a36.js"
+  }
+]);
